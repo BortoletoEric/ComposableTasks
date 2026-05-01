78f399c13a7e6a1ba5b2f0f7687701fd110f58ce
Bortoleto <ericbortoleto_9@outlook.com>
2026-05-01 00:22:02 -0300

    Refactor `RegisterActivity` to use reusable Compose components and update `RegisterViewModel` for state management.

--- app/src/main/java/com/example/composabletasks/view/RegisterActivity.kt
+++ app/src/main/java/com/example/composabletasks/view/RegisterActivity.kt
@@ -6,17 +6,25 @@ import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
-import com.example.composabletasks.R
+import com.example.composabletasks.view.components.CustomTextField
+import com.example.composabletasks.view.components.PrimaryButton
 import com.example.composabletasks.viewmodel.RegisterViewModel
 
 class RegisterActivity : AppCompatActivity() {
--- app/src/main/java/com/example/composabletasks/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/example/composabletasks/viewmodel/RegisterViewModel.kt
@@ -1,6 +1,9 @@
 package com.example.composabletasks.viewmodel
 
 import android.app.Application
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
